35269a1e4d828a6aa6f1d511f79d01dca7696eab
Zac Jones <zacjones93@gmail.com>
2019-08-01 16:43:16 -0400

    Update lesson 05 for codesandbox support

--- lessons/05-focus-management-using-css-html-and-javascript/script.js
+++ lessons/05-focus-management-using-css-html-and-javascript/script.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
+  var list = $("ul"),
+    listItems = list.find("li");
 
-	var list = $('ul'),
-		listItems = list.find('li');
-
-	$('.btn-delete').on('click', function() {
-		$(this).parent().remove();
-		listItems.find('.btn-delete').first().focus();
-	});
+  $(".btn-delete").on("click", function() {
+    $(this)
+      .parent()
+      .remove();
+    listItems
+      .find(".btn-delete")
+      .first()
+      .focus();
+  });
 });
